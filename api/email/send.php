d94dc9fe70f1e7ff31f5c92f749675909b72f253
Quentin Misslin <qmisslin@users.noreply.github.com>
2025-12-27 13:57:15 +0100

    feat: add email sending endpoint

---
+++ api/email/send.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/../../core/Boot.php';
+
+use Archetype\Core\APIHelper;
+use Archetype\Core\Email;
+use Archetype\Core\Auth; // Will be implemented next; temporarily disable the call
+use Archetype\Core\Router;
+
+/** @var Router $router */
+
+// TODO: Uncomment once Auth is implemented
+// Auth::check(['ADMIN', 'EDITOR']);
+
+$body = $router->getBody();
+
+if (empty($body['dest']) || empty($body['obj']) || empty($body['content'])) {
+    APIHelper::error('Missing fields: dest, obj, content');
+}
+
+$dest = is_array($body['dest']) ? $body['dest'] : explode(',', $body['dest']);
+$type = $body['type'] ?? 'text';
+
+try {
+    Email::send($dest, $body['obj'], $type, $body['content']);
+    APIHelper::success(['message' => 'Email sent']);
+} catch (\Throwable $e) {
+    APIHelper::error($e->getMessage(), 500);
+}
